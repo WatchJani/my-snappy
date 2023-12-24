1c663e2b7a51054ec7bfd883e9bd40ad5e4f2333
Janko <45310863+WatchJani@users.noreply.github.com>
2023-12-24 16:06:36 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"root/hash"
 )
 
 func main() {
 	hash := hash.New(1 << 14)
 	text := []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-
-	hash.Append(14, 1)
-	hash.Append(15, 2)
-
-	data, _ := hash.GetValue(14)
-	fmt.Println(data)
-
-	data, _ = hash.GetValue(15)
-	fmt.Println(data)
-
-	Search(text, hash)
+	buffer := make([]byte, 2000)
+	Search(buffer, text, hash)
 }
 
-func Search(data []byte, hash *hash.Hash) {
+func Search(buffer, data []byte, hash *hash.Hash) (d int) {
 	pointer := 0
 
 	for {
 		skip := 32
-		// var position uint32
+		var position int
 		for {
 			//quick search (first 32 byte check every, after 32 check
 			//every second, after that every third...)
@@ -41,20 +31,84 @@ func Search(data []byte, hash *hash.Hash) {
 			block_byte := load32(data, pointer) //4 byte per block
 
 			//check if our value exist
-			if _, find := hash.GetValue(block_byte); find {
-				// fmt.Println(p, pointer)
-				// position = p
-				// fmt.Println(string(data[p:p+4]), string(data[pointer:pointer+4]))
-				break
+			if p, find := hash.GetValue(block_byte); find { //p is candidate
+				if load32(data, p) == load32(data, pointer) {
+					position = p
+					break
+				}
 			}
 
 			//write position in hash map
 			hash.Append(block_byte, pointer)
 		}
+
+		d += emitLiteral(buffer, data[:position])
+
+		base := pointer
+
+		pointer, position = pointer+4, position+4
+
+		for pointer < len(data) {
+			if data[position] == data[pointer] {
+				position, pointer = position+1, pointer+1
+			} else {
+				break
+			}
+		}
+
+		d += emitCopy(buffer[d:], base-position, pointer-base)
 	}
 }
 
 func load32(b []byte, i int) uint32 {
 	b = b[i : i+4 : len(b)]
 	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+}
+
+func emitLiteral(dst, lit []byte) int {
+	i, n := 0, uint(len(lit)-1)
+	switch {
+	case n < 60:
+		dst[0] = uint8(n)<<2 | 0x00
+		i = 1
+	case n < 1<<8:
+		dst[0] = 60<<2 | 0x00
+		dst[1] = uint8(n)
+		i = 2
+	default:
+		dst[0] = 61<<2 | 0x00
+		dst[1] = uint8(n)
+		dst[2] = uint8(n >> 8)
+		i = 3
+	}
+	return i + copy(dst[i:], lit)
+}
+
+func emitCopy(dst []byte, offset, length int) int {
+	i := 0
+
+	for length >= 68 {
+		dst[i+0] = 63<<2 | 0x11
+		dst[i+1] = uint8(offset)
+		dst[i+2] = uint8(offset >> 8)
+		i += 3
+		length -= 64
+	}
+	if length > 64 {
+		dst[i+0] = 59<<2 | 0x11
+		dst[i+1] = uint8(offset)
+		dst[i+2] = uint8(offset >> 8)
+		i += 3
+		length -= 60
+	}
+	if length >= 12 || offset >= 2048 {
+		dst[i+0] = uint8(length-1)<<2 | 0x11
+		dst[i+1] = uint8(offset)
+		dst[i+2] = uint8(offset >> 8)
+		return i + 3
+	}
+
+	dst[i+0] = uint8(offset>>8)<<5 | uint8(length-4)<<2 | 0x01
+	dst[i+1] = uint8(offset)
+	return i + 2
 }
--- main_test.go
+++ main_test.go
@@ -8,8 +8,44 @@ import (
 func Benchmark(b *testing.B) {
 	hash := hash.New(1 << 14)
 	text := []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+	buffer := make([]byte, 1000)
 
 	for i := 0; i < b.N; i++ {
-		Search(text, hash)
+		Search(buffer, text, hash)
+	}
+}
+
+func BenchmarkEqual(b *testing.B) {
+	data := []byte("danas je lijep dana da je to super")
+	position, pointer := 0, 0
+
+	for i := 0; i < b.N; i++ {
+		for pointer < len(data) {
+			if data[position] == data[pointer] {
+				position, pointer = position+1, pointer+1
+			} else {
+				break
+			}
+		}
+	}
+}
+
+func BenchmarkEqual2(b *testing.B) {
+	data := []byte("danas je lijep dana da je to super")
+	position, pointer := 0, 0
+
+	for i := 0; i < b.N; i++ {
+		for b := position + 4; pointer < len(data) && data[b] == data[pointer]; b, pointer = b+1, pointer+1 {
+		}
+	}
+}
+
+func BenchmarkWriteNumber2(b *testing.B) {
+	data := []byte("danas je lijep dana da je to super")
+
+	buff := make([]byte, 8)
+
+	for i := 0; i < b.N; i++ {
+		emitLiteral(buff, data)
 	}
 }
